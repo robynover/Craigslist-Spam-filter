3148ac09d53da9b92f68edbae99128d02cb95075
Robyn Overstreet <robynover@gmail.com>
2012-02-19 15:48:04 -0500

    no, the REAL greasemonkey file

--- cl.user.js
+++ cl.user.js
@@ -6,8 +6,27 @@
 // @include       http://*.craigslist.org/*
 
 // ==/UserScript==
-alert('Hello Greasemonkey');
+
 
 window.addEventListener("load", function(e) {
-  alert("Hello, load";)
+   //alert('greasmonkey script loaded');
+	var links = document.getElementsByTagName('a');
+	count = 0;
+	for (i=0;i<links.length;i++){	
+		title = links[i].innerHTML;
+		total_chars = title.length;
+		caps = title.match(/[A-Z]/g);
+		if (caps){	
+			num_caps = caps.length;
+			percent_caps = num_caps/total_chars * 100;
+			if (percent_caps > 25){
+				//remove this listing
+				p = links[i].parentNode;
+				p.parentNode.removeChild(p);
+				//console.log (title + ' : removed');
+				count++;
+			}
+		}
+	}
+	//console.log(count+' spammy posts zapped!');
 }, false);
