64040642a70e4eb8f38da391af8bf10bcd715d20
Robyn Overstreet <robynover@gmail.com>
2012-02-19 15:37:33 -0500

    added greasemonkey script

---
+++ cl.user.js
@@ -0,0 +1,13 @@
+//greasemonkey script for CL
+// ==UserScript==
+// @name          CL Spam Filter
+
+// @description	  Hides spammy craigslist posts
+// @include       http://*.craigslist.org/*
+
+// ==/UserScript==
+alert('Hello Greasemonkey');
+
+window.addEventListener("load", function(e) {
+  alert("Hello, load";)
+}, false);
