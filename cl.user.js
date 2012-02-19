fc7f7c6e2b897acbb8ad2233a1f3ba3d4a84df64
Robyn Overstreet <robynover@gmail.com>
2012-02-19 16:03:50 -0500

    Update greasemonkey script to not destroy CL navigation links

--- cl.user.js
+++ cl.user.js
@@ -9,24 +9,28 @@
 
 
 window.addEventListener("load", function(e) {
-   //alert('greasmonkey script loaded');
+   //alert('greasemonkey script loaded');
 	var links = document.getElementsByTagName('a');
 	count = 0;
-	for (i=0;i<links.length;i++){	
-		title = links[i].innerHTML;
-		total_chars = title.length;
-		caps = title.match(/[A-Z]/g);
-		if (caps){	
-			num_caps = caps.length;
-			percent_caps = num_caps/total_chars * 100;
-			if (percent_caps > 25){
-				//remove this listing
-				p = links[i].parentNode;
-				p.parentNode.removeChild(p);
-				//console.log (title + ' : removed');
-				count++;
-			}
+	for (i=0;i<links.length;i++){
+		//we are only concerned with links inside <p> tags
+		if (links[i].parentNode.tagName == 'P'){
+			title = links[i].innerHTML;
+			total_chars = title.length;
+			caps = title.match(/[A-Z]/g);
+			if (caps){	
+				num_caps = caps.length;
+				percent_caps = num_caps/total_chars * 100;
+				if (percent_caps > 25){
+					//remove this listing
+					p = links[i].parentNode;
+					//p.parentNode.removeChild(p);
+					//console.log (title + ' : removed');
+					count++;
+				}
+			}	
 		}
+		
 	}
 	//console.log(count+' spammy posts zapped!');
 }, false);
